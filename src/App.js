6b4b4eda964be28adceb70092e2a99acd5a38853
kylelqtam <st.kaikai0.0@gmail.com>
2018-06-13 16:03:42 -0400

    move out

--- src/App.js
+++ src/App.js
@@ -2,136 +2,137 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import './css/main.css'
+import background from './img/allegro2018/569A2331.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="wrapper" class="fade-in">
+        <div id="wrapper" className="fade-in" style={{backgroundImage:`url(${background})`}}>
           <div id="intro">
             <h1>This is<br />
               Massively</h1>
             <p>A free, fully responsive HTML5 + CSS3 site template designed by <a href="https://html5up.net">HTML5 UP</a><br />
               and distributed by <a href="https://themewagon.com/">ThemeWagon</a>.</p>
-            <ul class="actions">
-              <li><a href="#header" class="button icon solo fa-arrow-down scrolly">Continue</a></li>
+            <ul className="actions">
+              <li><a href="#header" className="button icon solo fa-arrow-down scrolly">Continue</a></li>
             </ul>
           </div>
           <header id="header">
-            <a href="index.html" class="logo">Massively</a>
+            <a href="index.html" className="logo">Massively</a>
           </header>
           <nav id="nav">
-            <ul class="links">
-              <li class="active"><a href="index.html">This is Massively</a></li>
+            <ul className="links">
+              <li className="active"><a href="index.html">This is Massively</a></li>
               <li><a href="generic.html">Generic Page</a></li>
               <li><a href="elements.html">Elements Reference</a></li>
             </ul>
-            <ul class="icons">
-              <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-              <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-              <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-              <li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+            <ul className="icons">
+              <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+              <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+              <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+              <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
             </ul>
           </nav>
           <div id="main">
-            <article class="post featured">
-              <header class="major">
-                <span class="date">April 25, 2017</span>
+            <article className="post featured">
+              <header className="major">
+                <span className="date">April 25, 2017</span>
                 <h2><a href="#">And this is a<br />
                   massive headline</a></h2>
                 <p>Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
                   facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
                   amet nullam sed etiam veroeros.</p>
               </header>
-              <a href="#" class="image main"><img src="./img/pic01.jpg" alt="" /></a>
-              <ul class="actions">
-                <li><a href="#" class="button big">Full Story</a></li>
+              <a href="#" className="image main"><img src="./img/pic01.jpg" alt="" /></a>
+              <ul className="actions">
+                <li><a href="#" className="button big">Full Story</a></li>
               </ul>
             </article>
-            <section class="posts">
+            <section className="posts">
               <article>
                 <header>
-                  <span class="date">April 24, 2017</span>
+                  <span className="date">April 24, 2017</span>
                   <h2><a href="#">Sed magna<br />
                     ipsum faucibus</a></h2>
                 </header>
-                <a href="#" class="image fit"><img src="/allegro/src/img/pic02.jpg" alt="" /></a>
+                <a href="#" className="image fit"><img src="/allegro/src/img/pic02.jpg" alt="" /></a>
                 <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-                <ul class="actions">
-                  <li><a href="#" class="button">Full Story</a></li>
+                <ul className="actions">
+                  <li><a href="#" className="button">Full Story</a></li>
                 </ul>
               </article>
               <article>
                 <header>
-                  <span class="date">April 22, 2017</span>
+                  <span className="date">April 22, 2017</span>
                   <h2><a href="#">Primis eget<br />
                     imperdiet lorem</a></h2>
                 </header>
-                <a href="#" class="image fit"><img src="/allegro/src/img/pic03.jpg" alt="" /></a>
+                <a href="#" className="image fit"><img src="/allegro/src/img/pic03.jpg" alt="" /></a>
                 <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-                <ul class="actions">
-                  <li><a href="#" class="button">Full Story</a></li>
+                <ul className="actions">
+                  <li><a href="#" className="button">Full Story</a></li>
                 </ul>
               </article>
               <article>
                 <header>
-                  <span class="date">April 18, 2017</span>
+                  <span className="date">April 18, 2017</span>
                   <h2><a href="#">Ante mattis<br />
                     interdum dolor</a></h2>
                 </header>
-                <a href="#" class="image fit"><img src="./img/pic04.jpg" alt="" /></a>
+                <a href="#" className="image fit"><img src="./img/pic04.jpg" alt="" /></a>
                 <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-                <ul class="actions">
-                  <li><a href="#" class="button">Full Story</a></li>
+                <ul className="actions">
+                  <li><a href="#" className="button">Full Story</a></li>
                 </ul>
               </article>
               <article>
                 <header>
-                  <span class="date">April 14, 2017</span>
+                  <span className="date">April 14, 2017</span>
                   <h2><a href="#">Tempus sed<br />
                     nulla imperdiet</a></h2>
                 </header>
-                <a href="#" class="image fit"><img src="./img/pic05.jpg" alt="" /></a>
+                <a href="#" className="image fit"><img src="./img/pic05.jpg" alt="" /></a>
                 <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-                <ul class="actions">
-                  <li><a href="#" class="button">Full Story</a></li>
+                <ul className="actions">
+                  <li><a href="#" className="button">Full Story</a></li>
                 </ul>
               </article>
               <article>
                 <header>
-                  <span class="date">April 11, 2017</span>
+                  <span className="date">April 11, 2017</span>
                   <h2><a href="#">Odio magna<br />
                     sed consectetur</a></h2>
                 </header>
-                <a href="#" class="image fit"><img src="./img/pic06.jpg" alt="" /></a>
+                <a href="#" className="image fit"><img src="./img/pic06.jpg" alt="" /></a>
                 <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-                <ul class="actions">
-                  <li><a href="#" class="button">Full Story</a></li>
+                <ul className="actions">
+                  <li><a href="#" className="button">Full Story</a></li>
                 </ul>
               </article>
               <article>
                 <header>
-                  <span class="date">April 7, 2017</span>
+                  <span className="date">April 7, 2017</span>
                   <h2><a href="#">Augue lorem<br />
                     primis vestibulum</a></h2>
                 </header>
-                <a href="#" class="image fit"><img src="./img/pic07.jpg" alt="" /></a>
+                <a href="#" className="image fit"><img src="./img/pic07.jpg" alt="" /></a>
                 <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-                <ul class="actions">
-                  <li><a href="#" class="button">Full Story</a></li>
+                <ul className="actions">
+                  <li><a href="#" className="button">Full Story</a></li>
                 </ul>
               </article>
             </section>
             <footer>
-              <div class="pagination">
-                <a href="#" class="page active">1</a>
-                <a href="#" class="page">2</a>
-                <a href="#" class="page">3</a>
-                <span class="extra">&hellip;</span>
-                <a href="#" class="page">8</a>
-                <a href="#" class="page">9</a>
-                <a href="#" class="page">10</a>
-                <a href="#" class="next">Next</a>
+              <div className="pagination">
+                <a href="#" className="page active">1</a>
+                <a href="#" className="page">2</a>
+                <a href="#" className="page">3</a>
+                <span className="extra">&hellip;</span>
+                <a href="#" className="page">8</a>
+                <a href="#" className="page">9</a>
+                <a href="#" className="page">10</a>
+                <a href="#" className="next">Next</a>
               </div>
             </footer>
 
